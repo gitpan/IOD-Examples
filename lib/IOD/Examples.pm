1691080576c0915c34f705bff2a814c57ab53856
Steven Haryanto <stevenharyanto@gmail.com>
2014-08-17 12:57:49 +0000

    Import of SHARYANTO/IOD-Examples-0.01 from CPAN.
    gitpan-cpan-distribution: IOD-Examples
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SHARYANTO/IOD-Examples-0.01.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/IOD/Examples.pm
@@ -0,0 +1,50 @@
+package IOD::Examples;
+
+our $DATE = '2014-08-17'; # DATE
+our $VERSION = '0.01'; # VERSION
+
+1;
+# ABSTRACT: Example .IOD configuration files
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+IOD::Examples - Example .IOD configuration files
+
+=head1 VERSION
+
+This document describes version 0.01 of IOD::Examples (from Perl distribution IOD-Examples), released on 2014-08-17.
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/IOD-Examples>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/sharyanto/perl-IOD-Examples>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website L<https://rt.cpan.org/Public/Dist/Display.html?Name=IOD-Examples>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
